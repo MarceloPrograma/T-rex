d49f45d571f99d731c25ee7719848bc3bc74fece
MarceloPrograma <91920993+MarceloPrograma@users.noreply.github.com>
2021-10-04 17:36:09 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,184 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloud, cloudsGroup, cloudImage;
+
+var obstacle, obstacleGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+var restart,restartImage;
+var gameOver, gameOverImage;
+
+
+var score=0;
+
+var dieSound, jumpSound,checkPointSound;
+
+var newImage;
+
+var END=0
+
+var PLAY=1;
+
+var gameState=PLAY; 
+      
+  
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+    dieSound = loadSound("die.mp3");
+    jumpSound = loadSound("jump.mp3");
+    checkPointSound = loadSound("check_point_sonic.mp3");
+     restartImage = loadImage("restart.png");  
+      gameOverImage = loadImage("gameOver.png"); 
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  
+  obstacle2 = loadImage("obstacle2.png");
+  
+  obstacle3 = loadImage("obstacle3.png");
+  
+  obstacle4 = loadImage("obstacle4.png");
+  
+  obstacle5 = loadImage("obstacle5.png");
+  
+  obstacle6 = loadImage("obstacle6.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided",trex_collided)
+  trex.setCollider("circle", -15,0,40);
+  trex.debug=true;
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  
+  restart = createSprite(300,140,40,40);
+  restart.addImage("restart",restartImage);
+  restart.visible = false;
+  gameOver = createSprite(300,80,40,20);
+  gameOver.addImage("gameOver",gameOverImage);
+  gameOver.visible = false;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  console.log("Hello"+ 5)
+  obstacleGroup=new Group();
+  cloudsGroup=new Group();
+}
+
+function draw() {
+  background(180);
+  text("Score: "+score,450,50)
+  if(gameState==PLAY){
+    score=score+Math.round(getFrameRate()/60);
+  if(score>0 && score%100==0){
+  checkPointSound.play();  
+  }
+  if(keyDown("space")&& trex.y >= 150) {
+    trex.velocityY = -13;
+    jumpSound.play();
+  }
+    
+  ground.velocityX = -4-3*score/100;
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+      spawnClouds();
+  spawnObstacles(); 
+     if (obstacleGroup.isTouching(trex)){
+   gameState=END; 
+   dieSound.play();    
+    }
+  }
+  
+  else if(gameState==END){
+    obstacleGroup.setVelocityXEach(0)
+    cloudsGroup.setVelocityXEach(0)
+   ground.velocityX=0
+     trex.velocityX=0
+      trex.velocityY=0
+    trex.changeAnimation("collided",trex_collided);
+    gameOver.visible = true;
+    restart.visible = true;
+  }
+    
+  trex.collide(invisibleGround);
+  
+  //aparece las nubes
+if(mousePressedOver(restart)){
+  reset();
+}
+  drawSprites();
+}
+function reset(){
+  gameState = PLAY;
+  gameOver.visible = false;
+  restart.visible = false;
+  score = 0;
+  obstacleGroup.destroyEach();
+  cloudsGroup.destroyEach();
+  trex.changeAnimation("running", trex_running);
+}
+function spawnClouds() {
+  //escribe el código aquí para aparecer las nubes
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage)
+    cloud.y = Math.round(random(10,60))
+    cloud.scale = 0.4;
+    cloud.velocityX = -3;
+    cloudsGroup.add(cloud);
+    
+    //asigna ciclo de vida a la variable
+    cloud.lifetime = 200
+    
+    //ajusta la profundidad
+    cloud.depth = trex.depth
+    trex.depth = trex.depth + 1;
+  }
+}
+function spawnObstacles() {
+  if (frameCount % 60 === 0) {
+    obstacle = createSprite(600,165,10,40);
+    obstacle.scale = 0.4
+    obstacle.velocityX = -6-score/100 
+    var rand = Math.round(random(1,6))
+    switch(rand){
+      case 1: obstacle.addImage(obstacle1);
+        break;
+        
+        case 2: obstacle.addImage(obstacle2);
+        break;
+        
+        case 3: obstacle.addImage(obstacle3);
+        break;
+        
+        case 4: obstacle.addImage(obstacle4);
+        break;
+        
+        case 5: obstacle.addImage(obstacle5);
+        break;
+        
+        case 6: obstacle.addImage(obstacle6);
+      default: break;
+        
+    }
+    obstacleGroup.add(obstacle)
+
+  }
+}
+
